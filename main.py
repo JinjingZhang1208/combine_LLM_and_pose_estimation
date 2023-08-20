1db3ebccb259447af1aa828c5dda0d405497a6ca
Hongyu Wan <85655086+hongyuwan@users.noreply.github.com>
2023-08-21 00:01:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ import random
 import time
 
 sys.path.append('../python-osc/')
-
+print(sd.query_devices())
 # OpenAI API key and Google Cloud credentials setup
 openai.api_key = '*'
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '*.json'
