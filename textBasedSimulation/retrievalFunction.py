6aeb4b73f28028eab6fb317c370b7b1888b17add
abdulaziz <suria.abdul04@gmail.com>
2023-09-24 16:03:54 -0400

    improving the current system

--- textBasedSimulation/retrievalFunction.py
+++ textBasedSimulation/retrievalFunction.py
@@ -1,50 +1,30 @@
+from heapq import heapify, heappushpop
 from sentence_transformers import SentenceTransformer, util
-model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
+
+model = SentenceTransformer("paraphrase-MiniLM-L6-v2")
 # Recency : not needed mostly...
 
-def retrievalFunction(currentConversation:str,memoryStream:list):
-    return calculateRelevance(currentConversation,memoryStream)
 
-def calculateRelevance(currentConversation:str, memoryStream:list):
-    content_embedding = model.encode(currentConversation, convert_to_tensor=True)
-    observationData=[]
-    for memory in memoryStream:
-        for observation in memory['Observations']:
-            data_embedding = model.encode(observation, convert_to_tensor=True)
-            similarity = util.pytorch_cos_sim(content_embedding, data_embedding).item()
-            observationData.append({'observationText':observation,'relevance':similarity})
-    mostRelevantData=sorted(observationData,key=lambda x : x['relevance'],reverse=True)
-    return mostRelevantData[:5] 
-
-"""
-def calculateImportance(content,stored_memory_data):
-    importancePrompt ={
-        "context": f"You are a conversational agent who will is provided with a set of observation strings. Determine the most important observations based on the current conversation.",
-        "information": {
-            "description":"Provide an integer on a scale of 1 to 10 where 1 is purely mundane and 10 is extremely poigant. rate the likely poigancy of the following observation strings"
-        }
-    }
+def retrievalFunction(currentConversation: str, memoryStream: list):
+    return calculateRelevance(currentConversation, memoryStream)
 
 
+def calculateRelevance(currentConversation: str, memoryStream: list):
+    contentEmbedding = model.encode(currentConversation, convert_to_tensor=True)
+    memoryData = [
+        (memory, observation)
+        for memory in memoryStream
+        for observation in memory["Observations"]
+    ]
+    observationData = [observation for memory, observation in memoryData]
+    dataEmbedding = model.encode(observationData, convert_to_tensor=True)
+    similarityVector = util.pytorch_cos_sim(contentEmbedding, dataEmbedding).tolist()[0]
+    relevantObservations = [(float("-inf"), "") for i in range(3)]
+    heapify(relevantObservations)
+    for idx, simScore in enumerate(similarityVector):
+        relevance = simScore
+        currObservation = (relevance, observationData[idx])
+        if relevance > relevantObservations[0][0]:
+            heappushpop(relevantObservations, currObservation)
 
-
-def createBasePrompt(userName,baseDescription):
-
-    BACKGROUND_DESCRIPTION_CONTEXT=f"You are the user {userName}. You wil be provided with a description with background details about you."
-
-    # Create a dictionary that represents the JSON structure
-    prompt = {
-        "context": BACKGROUND_DESCRIPTION_CONTEXT,
-        "information": {
-            "description":baseDescription
-        },
-        "criteria": [
-            "Generate a list consisting of all the important observations made from the description",
-            "Each item in the list should consist of one observation separated by a new line"
-        ],
-        "example": "(Observation 1)\n(Observation 2)\n"
-    }
-    return json.dumps(prompt,indent=4)
-"""
-
-
+    return sorted(relevantObservations, key=lambda x: x[0], reverse=True)
--- textBasedSimulation/textBasedSimulation.py
+++ textBasedSimulation/textBasedSimulation.py
@@ -1,130 +1,207 @@
 import time
 import os
+import datetime
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
+from collections import deque
 from dotenv import load_dotenv
 from retrievalFunction import retrievalFunction
-import datetime
 from pymongo.mongo_client import MongoClient
-from responseGenerator import generateInitialObservations,generateObservations,generateConversation 
+from responseGenerator import (
+    generateInitialObservations,
+    generateObservations,
+    generateConversation,
+)
 
 load_dotenv()
 
 # Constants
-DATABASE_NAME='LLMDatabase'
-DATABASE_URL=os.environ.get('DATABASE_URL') 
-COLLECTION_USERS='Users'
-COLLECTION_MEMORY_OBJECTS='MemoryObjects'
+DATABASE_NAME = "LLMDatabase"
+DATABASE_URL = os.environ.get("DATABASE_URL")
+COLLECTION_USERS = "Users"
+COLLECTION_MEMORY_OBJECTS = "MemoryObjects"
 
 # Basic objects for the Database.
-client= MongoClient(DATABASE_URL)
-LLMdatabase= client[DATABASE_NAME]
-userCollection= LLMdatabase[COLLECTION_USERS]
-memoryObjectCollection=LLMdatabase[COLLECTION_MEMORY_OBJECTS]
+client = MongoClient(DATABASE_URL)
+LLMdatabase = client[DATABASE_NAME]
+userCollection = LLMdatabase[COLLECTION_USERS]
+memoryObjectCollection = LLMdatabase[COLLECTION_MEMORY_OBJECTS]
 
-def fetchPastRecords(userName,memoryObjectCollection):
-    fetchQuery= {
-    '$or': [
-        {'Username': userName},
-        {'Conversation with User': userName}
-    ]
+
+# Fetch the base description once.
+def fetchBaseDescription(userName: str):
+    return deque(
+        memoryObjectCollection.find(
+            {"Username": userName, "Conversation with User": "Base Description"}
+        ),
+    )
+
+
+# fetch the past records once.
+def fetchPastRecords(userName: str):
+    fetchQuery = {
+        "$or": [{"Username": userName}, {"Conversation with User": userName}],
+        "Conversation with User": {"$ne": "Base Description"},
     }
-    return list(memoryObjectCollection.find(fetchQuery).limit(10))
+    return deque(
+        memoryObjectCollection.find(fetchQuery).sort("_id", -1).limit(15), maxlen=15
+    )
 
-def updateMemoryCollection(userName,conversationalUser,observationList):
+
+def updateBaseDescription(userName: str, observationList: list):
     # Get the current time.
-    currTime=datetime.datetime.now()
-    formattedTime=currTime.strftime("%Y-%m-%d %H:%M:%S")
+    currTime = datetime.datetime.now()
+    formattedTime = currTime.strftime("%Y-%m-%d %H:%M:%S")
     # Update the memoryObjects collection.
-    memoryObjectData={
-        "Username":userName,
-        "Conversation with User": conversationalUser,
-        "Creation Time":formattedTime,
-        "Recent Access Time":formattedTime,
-        "Observations":observationList
+    memoryObjectData = {
+        "Username": userName,
+        "Conversation with User": "Base Description",
+        "Creation Time": formattedTime,
+        "Recent Access Time": formattedTime,
+        "Observations": observationList,
     }
+    # Update the latest collection with the id parameter and insert to the database.
     memoryObjectCollection.insert_one(memoryObjectData)
+    # Delete the oldest record and add the latest one.
+
+
+def updateMemoryCollection(
+    userName: str, conversationalUser: str, observationList: list
+):
+    global pastObservations
+    # Get the current time.
+    currTime = datetime.datetime.now()
+    formattedTime = currTime.strftime("%Y-%m-%d %H:%M:%S")
+    # Update the memoryObjects collection.
+    memoryObjectData = {
+        "Username": userName,
+        "Conversation with User": conversationalUser,
+        "Creation Time": formattedTime,
+        "Recent Access Time": formattedTime,
+        "Observations": observationList,
+    }
+    # Update the latest collection with the id parameter and insert to the database.
+    currentObject = memoryObjectCollection.insert_one(memoryObjectData)
+    memoryObjectData["_id"] = currentObject.inserted_id
+    # Delete the oldest record and add the latest one.
+    if len(pastObservations) > 15:
+        pastObservations.pop()
+    pastObservations.appendleft(memoryObjectData)
+
 
 def getBaseDescription():
-    description=""
+    description = ""
     while True:
-        currLine=input("Please enter a relevant description about your character. Type done to complete the description \n")
-        if currLine.lower()=="done":
+        currLine = input(
+            "Please enter a relevant description about your character. Type done to complete the description \n"
+        )
+        if currLine.lower() == "done":
             break
-        description+=f"{currLine}\n"
+        description += f"{currLine}\n"
     return description
 
+
 def startConversation(userName):
-
-    conversationalUser=input("Define the username you are having a conversation with : ")
+    global pastObservations
+    conversationalUser = input("Define the username you are acting as: ")
+    baseObservation = fetchBaseDescription(userName)
+    pastObservations = fetchPastRecords(userName)
+    eventLoop = asyncio.get_event_loop()
+    threadExecutor = ThreadPoolExecutor()
     while True:
-        currentConversation=input(f"Talk with {userName}, You are {conversationalUser}. Have a discussion! ")
-        if currentConversation.lower()=="done":
+        currentConversation = input(
+            f"Talk with {userName}, You are {conversationalUser}. Have a discussion! "
+        )
+        if currentConversation.lower() == "done":
             break
-        pastObservations=fetchPastRecords(userName,memoryObjectCollection)
-        # Delete and add 1 latest record.
+        retreivalResults = retrievalFunction(
+            currentConversation, baseObservation + pastObservations
+        )
+        startTime = time.time()
+        conversationPrompt = generateConversation(
+            userName,
+            conversationalUser,
+            currentConversation,
+            [data[1] for data in retreivalResults],
+        )
+        print(f"{userName} :")
+        resultConversationString = ""
+        for conversation in conversationPrompt:
+            try:
+                currText = conversation["choices"][0]["delta"]["content"]
+                resultConversationString += currText
+                print(currText, end="")
+            except:
+                break
+        print()
+        endTime = time.time()
+        print(
+            f"Time taken for the conversation generation by GPT : {endTime-startTime:.2f}"
+        )
+        eventLoop.run_in_executor(
+            threadExecutor,
+            generateObservationAndUpdateMemory,
+            userName,
+            conversationalUser,
+            currentConversation,
+            resultConversationString,
+        )
 
-        retreivalResults=retrievalFunction(currentConversation,pastObservations)
-        startTime=time.time()
-        finalStatement=generateConversation(userName,conversationalUser,currentConversation,[ data['observationText'] for data in retreivalResults])
-        endTime=time.time()
-        print(f"Time taken for the conversation generation by GPT : {endTime-startTime:.2f}")
-        print(f"{userName} : {finalStatement}")
 
+def generateObservationAndUpdateMemory(
+    userName, conversationalUser, currentConversation, resultConversationString
+):
+    time.sleep(10)
+    # Time the function call and fetch the results.
+    startTime = time.time()
+    observationList = generateObservations(
+        userName, conversationalUser, currentConversation, resultConversationString
+    )
+    observationList = observationList.split("\n")
+    finalObservations = []
+    for observation in observationList:
+        if len(observation) > 0:
+            finalObservations.append(observation)
+
+    endTime = time.time()
+    """
+    print(
+        f"Time taken for the observation generation by GPT : {endTime-startTime:.2f} "
+    )
+    """
+
+    updateMemoryCollection(userName, conversationalUser, finalObservations)
+
+
+if __name__ == "__main__":
+    pastObservations = deque()
+    # Get username.
+    userName = input("Please enter the username of character: ")
+
+    # Check for existing user.
+    existingUser = userCollection.find_one({"Username": userName})
+
+    if existingUser:
+        print(f"Welcome back! {userName} \nContinue where you left off")
+    else:
+        # Collect the description details.
+        description = getBaseDescription()
+
+        # Insert the userData to the Users collection.
+        userData = {"Username": userName, "Description": description}
+        userCollection.insert_one(userData)
 
         # Time the function call and fetch the results.
-        startTime=time.time()
-        observationList=generateObservations(userName,conversationalUser,currentConversation,finalStatement).split('\n')
-        observationList=[observation.strip() for observation in observationList]
-        endTime=time.time()
-        print(f"Time taken for the observation generation by GPT : {endTime-startTime:.2f} ")
+        startTime = time.time()
+        observationList = generateInitialObservations(userName, description).split("\n")
+        endTime = time.time()
+        print(
+            f"Time taken for the observation generation by GPT : {endTime-startTime:.2f} "
+        )
 
-        updateMemoryCollection(userName,conversationalUser,observationList)
+        # Generate the memory object data and push it to the memory objects collection.
+        updateBaseDescription(userName, observationList)
+        print("User created successfully!")
 
-
-
-
-# Get username.
-userName=input("Please enter a username : ")
-
-# Check for existing user.
-existingUser=userCollection.find_one({"Username":userName})
-
-if existingUser:
-    print(f"Welcome back! {userName} \nContinue where you left off")
     startConversation(userName)
-
-    """
-    iterate through each record and add three new scores normalized.
-    Evaluate the importance of each set of observations based on the given info 
-    And calculate the most relevant one
-    """
-    """
-    OR 
-    pick the ones with highest relevance and based on that make a judgement for the remaining ones
-    """
-else:
-
-    # Collect the description details.
-    description=getBaseDescription()
-    
-    # Insert the userData to the Users collection.
-    userData= {
-        "Username": userName,
-        "Description":description
-    }
-    userCollection.insert_one(userData)
-
-
-    # Time the function call and fetch the results.
-    startTime=time.time()
-    observationList=generateInitialObservations(userName,description).split('\n')
-    endTime=time.time()
-    print(f"Time taken for the observation generation by GPT : {endTime-startTime:.2f} ")
-
-    # Generate the memory object data and push it to the memory objects collection.
-    updateMemoryCollection(userName,"Base Description",observationList)
-    print("User created successfully!")
-    startConversation(userName)
-
-
     client.close()
-
