d1feac5f16bbc09bc060eb11e53a5000655fff12
hongyuwan <85655086+hongyuwan@users.noreply.github.com>
2023-09-26 00:54:51 +0800

    OCR+simulator working in VRC

---
+++ OCR/Models/TextTranslation/texttranslate.py
@@ -0,0 +1,78 @@
+import settings
+import pykakasi
+# import texttranslateM2M100
+from Models.TextTranslation import texttranslateM2M100_CTranslate2
+from Models.TextTranslation import texttranslateNLLB200
+from Models.TextTranslation import texttranslateNLLB200_CTranslate2
+
+
+def get_current_translator():
+    return settings.GetOption("txt_translator")
+
+
+def iso3_to_iso1(iso3_code):
+    for iso1, iso3_codes in texttranslateNLLB200_CTranslate2.LANGUAGES_ISO1_TO_ISO3.items():
+        if iso3_code in iso3_codes:
+            return iso1
+    return None
+
+
+def convert_to_romaji(text):
+    # Convert Hiragana, Katakana, Japanese to romaji (ascii compatible)
+    kks = pykakasi.kakasi()
+    converted_text = kks.convert(text)
+    full_converted_text = []
+    for converted_text_item in converted_text:
+        full_converted_text.append(converted_text_item['hepburn'])
+    return ' '.join(full_converted_text)
+
+
+# Download and install Translate packages
+def InstallLanguages():
+    match get_current_translator():
+        case "M2M100":
+            texttranslateM2M100_CTranslate2.load_model(settings.GetOption("txt_translator_size"), compute_type=settings.GetOption("txt_translator_precision"))
+        case "NLLB200":
+            texttranslateNLLB200.load_model(settings.GetOption("txt_translator_size"), compute_type=settings.GetOption("txt_translator_precision"))
+        case "NLLB200_CT2":
+            texttranslateNLLB200_CTranslate2.load_model(settings.GetOption("txt_translator_size"), compute_type=settings.GetOption("txt_translator_precision"))
+
+
+def GetInstalledLanguageNames():
+    match get_current_translator():
+        case "M2M100":
+            return texttranslateM2M100_CTranslate2.get_installed_language_names()
+        case "NLLB200":
+            return texttranslateNLLB200.get_installed_language_names()
+        case "NLLB200_CT2":
+            return texttranslateNLLB200_CTranslate2.get_installed_language_names()
+
+
+def TranslateLanguage(text, from_code, to_code, to_romaji=False, as_iso1=False):
+    translation_text = ""
+    match get_current_translator():
+        case "M2M100":
+            try:
+                translation_text = texttranslateM2M100_CTranslate2.translate_language(text, from_code, to_code)
+            except Exception as e:
+                print("Error: " + str(e))
+        case "NLLB200":
+            try:
+                translation_text, from_code, to_code = texttranslateNLLB200.translate_language(text, from_code, to_code, as_iso1)
+            except Exception as e:
+                print("Error: " + str(e))
+        case "NLLB200_CT2":
+            try:
+                translation_text, from_code, to_code = texttranslateNLLB200_CTranslate2.translate_language(text, from_code, to_code, as_iso1)
+            except Exception as e:
+                print("Error: " + str(e))
+    if to_romaji:
+        translation_text = convert_to_romaji(translation_text)
+
+    return translation_text.strip(), from_code, to_code
+
+
+def SetDevice(option):
+    texttranslateNLLB200.set_device(option)
+    texttranslateNLLB200_CTranslate2.set_device(option)
+    texttranslateM2M100_CTranslate2.set_device(option)
---
+++ OCR/textBasedSimulation.py
@@ -0,0 +1,143 @@
+import time
+import os
+from dotenv import load_dotenv
+from retrievalFunction import retrievalFunction
+import datetime
+from pymongo.mongo_client import MongoClient
+from responseGenerator import generateInitialObservations,generateObservations,generateConversation
+import sys
+# Add the path of the 'b' folder to sys.path
+import easyocr1
+import VRC_OSCLib
+DATABASE_URL="mongodb+srv://abdulaziz:5k9406K7@llmcluster.slofevi.mongodb.net/?retryWrites=true&w=majority";
+load_dotenv()
+# Constants
+DATABASE_NAME='LLMDatabase'
+DATABASE_URL=os.environ.get('DATABASE_URL') 
+COLLECTION_USERS='Users'
+COLLECTION_MEMORY_OBJECTS='MemoryObjects'
+
+# Basic objects for the Database.
+client= MongoClient(DATABASE_URL)
+LLMdatabase= client[DATABASE_NAME]
+userCollection= LLMdatabase[COLLECTION_USERS]
+memoryObjectCollection=LLMdatabase[COLLECTION_MEMORY_OBJECTS]
+x = memoryObjectCollection.find_one()
+print(x)
+def custom_print(*args, **kwargs):
+    # Print to console
+    print(*args, **kwargs)
+
+    # Write to log file
+    with open("output.log", "a") as log_file:
+        print(*args, file=log_file, **kwargs)
+
+def fetchPastRecords(userName,memoryObjectCollection):
+    fetchQuery= {
+    '$or': [
+        {'Username': userName},
+        {'Conversation with User': userName}
+    ]
+    }
+    return list(memoryObjectCollection.find(fetchQuery).limit(10))
+
+def updateMemoryCollection(userName,conversationalUser,observationList):
+    # Get the current time.
+    currTime=datetime.datetime.now()
+    formattedTime=currTime.strftime("%Y-%m-%d %H:%M:%S")
+    # Update the memoryObjects collection.
+    memoryObjectData={
+        "Username":userName,
+        "Conversation with User": conversationalUser,
+        "Creation Time":formattedTime,
+        "Recent Access Time":formattedTime,
+        "Observations":observationList
+    }
+    memoryObjectCollection.insert_one(memoryObjectData)
+
+def getBaseDescription():
+    description=""
+    while True:
+        currLine=input("Please enter a relevant description about your character. Type done to complete the description \n")
+        if currLine.lower()=="done":
+            break
+        description+=f"{currLine}\n"
+    return description
+
+def startConversation(userName):
+
+    conversationalUser=input("Define the username you are having a conversation with : ")
+    while True:
+        # currentConversation=input(f"Talk with {userName}, You are {conversationalUser}. Have a discussion! ")
+        currentConversation= easyocr1.run_image_processing("VRChat",["en"])
+        if currentConversation.lower()=="done":
+            break
+        pastObservations=fetchPastRecords(userName,memoryObjectCollection)
+        # Delete and add 1 latest record.
+
+        retreivalResults=retrievalFunction(currentConversation,pastObservations)
+        startTime=time.time()
+        finalStatement=generateConversation(userName,conversationalUser,currentConversation,[ data['observationText'] for data in retreivalResults])
+        endTime=time.time()
+        custom_print(f"Time taken for the conversation generation by GPT : {endTime-startTime:.2f}")
+        custom_print(f"{userName} : {finalStatement}")
+        VRC_OSCLib._direct_osc_send(data=finalStatement)
+
+        # Time the function call and fetch the results.
+        startTime=time.time()
+        observationList=generateObservations(userName,conversationalUser,currentConversation,finalStatement).split('\n')
+        observationList=[observation.strip() for observation in observationList]
+        endTime=time.time()
+        custom_print(f"Time taken for the observation generation by GPT : {endTime-startTime:.2f} ")
+
+        updateMemoryCollection(userName,conversationalUser,observationList)
+
+
+
+
+# Get username.
+userName=input("Please enter a username : ")
+
+# Check for existing user.
+existingUser=userCollection.find_one({"Username":userName})
+
+if existingUser:
+    custom_print(f"Welcome back! {userName} \nContinue where you left off")
+    startConversation(userName)
+
+    """
+    iterate through each record and add three new scores normalized.
+    Evaluate the importance of each set of observations based on the given info 
+    And calculate the most relevant one
+    """
+    """
+    OR 
+    pick the ones with highest relevance and based on that make a judgement for the remaining ones
+    """
+else:
+
+    # Collect the description details.
+    description=getBaseDescription()
+    
+    # Insert the userData to the Users collection.
+    userData= {
+        "Username": userName,
+        "Description":description
+    }
+    userCollection.insert_one(userData)
+
+
+    # Time the function call and fetch the results.
+    startTime=time.time()
+    observationList=generateInitialObservations(userName,description).split('\n')
+    endTime=time.time()
+    custom_print(f"Time taken for the observation generation by GPT : {endTime-startTime:.2f} ")
+
+    # Generate the memory object data and push it to the memory objects collection.
+    updateMemoryCollection(userName,"Base Description",observationList)
+    custom_print("User created successfully!")
+    startConversation(userName)
+
+
+    client.close()
+
