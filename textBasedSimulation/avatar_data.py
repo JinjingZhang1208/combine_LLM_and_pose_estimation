7795be9771af974ab205533ef32e19fda333cb53
abdulaziz <suria.abdul04@gmail.com>
2023-11-30 16:12:16 -0500

    Updated the avatar DB

---
+++ textBasedSimulation/avatar_data.py
@@ -0,0 +1,19 @@
+avatar_expression_map = {
+    "Happy": 2,
+    "Smug": 5,
+    "Wink": 4,
+    "Confused": 3,
+    "Excited": 6,
+    "Angry": 1,
+}
+avatar_action_map = {
+    "Wave Hands": 1,
+    "Clap": 2,
+    "Point": 3,
+    "Cheer": 4,
+    "Dance": 5,
+    "Backflip": 6,
+    "Sadness": 7,
+    "Faint": 8,
+}
+avatar_voice = "nova"
